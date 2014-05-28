2ed8ce8ed73a70d964a33a8c3e52d7559642270f
cranebird <cranebird@vdeb>
2014-05-28 10:21:11 +0900

    clean up code.

--- REPL.hs
+++ REPL.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 --
 -- REPL.hs
 --
@@ -7,59 +8,11 @@ import qualified System.Console.Haskeline as H
 import Control.Monad.Error
 import Control.Monad.IO.Class
 import Type
-import Read
 import Interp
 import System.IO
 
-main = repl2
-
--- repl :: IO () 
--- repl = H.runInputT H.defaultSettings loop
---   where
---     -- loop :: H.InputT IO ()
---     loop = do
---       minput <- H.getInputLine "REPL>>> "
---       case minput of
---         Nothing -> return ()
---         Just "quit" -> return ()
---         Just "" -> loop
---         Just input -> do
---           case readExp input of
---             Right v -> printExp (interp v)
---           loop
-
--- repl :: IO () 
-repl = H.runInputT H.defaultSettings loop
-
--- loop :: H.InputT IO ()
--- loop = do
---   minput <- H.getInputLine "REPL>>> "
---   case minput of
---     Nothing -> return ()
---     Just "quit" -> return ()
---     Just "" -> loop
---     Just input -> do
---       readAndEval input
---       loop
-
-loop :: H.InputT IO ()
-loop = undefined
-
--- loop' env = do
---   minput <- H.getInputLine "REPL>>> "
---   case minput of
---     Nothing -> return ()
---     Just "quit" -> return ()
---     Just "" -> loop
---     Just input -> do
---       readAndEval input
---       loop'
-
-
---readAndEval :: Control.Monad.IO.Class.MonadIO m => String -> H.InputT m ()
-readAndEval str = case readExp str of
-  Left err -> H.outputStrLn (show err)
-  Right v -> H.outputStrLn (show v)
+main :: IO ()
+main = repl
 
 -- original
 flushStr str = putStr str >> hFlush stdout
@@ -70,8 +23,34 @@ until_ pred prompt action = do
   if pred result
      then return ()
     else action result >> until_ pred prompt action
+-- Haskeline version.
+repl :: IO ()
+repl = do
+  env <- primitiveBindings
+  H.runInputT H.defaultSettings (loop env)
+  where
+    loop :: Env -> H.InputT IO ()
+    loop env = do
+      minput <- H.getInputLine "REPL>>> "
+      case minput of
+        Nothing -> return ()
+        Just "quit" -> return ()
+        Just input -> do
+          evalAndHaskelineOutput env input
+          loop env
 
-repl2 = primitiveBindings >>= 
-        until_ (== "quit") (readPrompt "REPL>>> ") . evalAndPrint
+evalAndHaskelineOutput :: Env -> String -> H.InputT IO ()
+evalAndHaskelineOutput env str = liftIO (evalString env str) >>= H.outputStrLn
 
-
+-- ghc 7.8.2 error;
+-- <interactive>:1274:23:
+--     No instance for (MonadError ScmError (H.InputT IO))
+--       arising from a use of ‘repl4’
+--     In the second argument of ‘(>>=)’, namely ‘repl4’
+--     In the expression: primitiveBindings >>= repl4
+--     In an equation for ‘it’: it = primitiveBindings >>= repl4
+-- *Main> :t repl4
+-- repl4
+--   :: (H.MonadException m, MonadError ScmError (H.InputT m),
+--       MonadIO (H.InputT m)) =>
+--      Env -> m ()
